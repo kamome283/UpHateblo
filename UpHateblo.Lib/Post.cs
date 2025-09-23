88a34777a7da058c115a2a2c8b8a2baaecdfb831
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-09-23 18:37:50 +0900

    add: draft post entry schema

--- UpHateblo.Lib/Post.cs
+++ UpHateblo.Lib/Post.cs
@@ -13,6 +13,55 @@ public record EntryHeader(
     string UrlPath
 );
 
+internal static class PostRequestSchema
+{
+    // Reference: https://developer.hatena.ne.jp/ja/documents/blog/apis/atom/
+    public class PostEntryRequestSchema(BlogConfig config, EntryHeader header, string content)
+    {
+        public readonly string Title = header.Title;
+        public readonly Author Author = new(config.Username);
+        public readonly Content Content = new(content);
+        public readonly DateTime Updated = header.Date;
+        public readonly Category Category = new(header.Category);
+
+        // No support for draft and preview post currently
+        [XmlArray("app:control")] public readonly AppControl AppControl = new(false, false);
+
+        [XmlArray("hatenablog:custom-url")]
+        public readonly CustomUrl CustomUrl = new(header.UrlPath);
+    }
+
+    public class Author(string name)
+    {
+        public readonly string Name = name;
+    }
+
+    public class Content(string content)
+    {
+        [XmlAttribute] public readonly string Type = "text/plain";
+        [XmlElement] public readonly string Element = content;
+    }
+
+    public class Category(string[] category)
+    {
+        [XmlAttribute] public readonly string[] Term = category;
+    }
+
+    public class AppControl(bool draft, bool preview)
+    {
+        [XmlArray("app:draft")] public readonly string Draft = draft ? "yes" : "no";
+        [XmlArray("app:preview")] public readonly string Preview = preview ? "yes" : "no";
+    }
+
+    public class CustomUrl(string urlPath)
+    {
+        [XmlAttribute("xmlns:hatenablog")]
+        public readonly string HatenaBlog = "http://www.hatena.ne.jp/info/xmlns#hatenablog";
+
+        [XmlElement] public readonly string Element = urlPath;
+    }
+}
+
 public static class Post
 {
     public static async Task Run(
