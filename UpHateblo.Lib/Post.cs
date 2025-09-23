fd13fe63f40d467b78449d47c232f5ef605399b5
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-09-23 19:27:48 +0900

    fix: post entry schema
    System.Xml.Serialization への無知からスキーマの定義のアトリビュートが異なっていた。
    また、Serializerにケバブケースに変えるなどのオプションが用意されていなかったので、すべてのプロパティにXMLでの要素名を用意した

--- UpHateblo.Lib/Post.cs
+++ UpHateblo.Lib/Post.cs
@@ -1,4 +1,6 @@
-﻿namespace UpHateblo.Lib;
+﻿using System.Xml.Serialization;
+
+namespace UpHateblo.Lib;
 
 public record BlogConfig(
     string BlogId,
@@ -16,41 +18,42 @@ public record EntryHeader(
 internal static class PostRequestSchema
 {
     // Reference: https://developer.hatena.ne.jp/ja/documents/blog/apis/atom/
+    [XmlRoot("entry")]
     public class PostEntryRequestSchema(BlogConfig config, EntryHeader header, string content)
     {
-        public readonly string Title = header.Title;
-        public readonly Author Author = new(config.Username);
-        public readonly Content Content = new(content);
-        public readonly DateTime Updated = header.Date;
-        public readonly Category Category = new(header.Category);
+        [XmlElement("title")] public readonly string Title = header.Title;
+        [XmlElement("author")] public readonly Author Author = new(config.Username);
+        [XmlElement("content")] public readonly Content Content = new(content);
+        [XmlElement("updated")] public readonly DateTime Updated = header.Date;
+        [XmlElement("category")] public readonly Category Category = new(header.Category);
 
         // No support for draft and preview post currently
-        [XmlArray("app:control")] public readonly AppControl AppControl = new(false, false);
+        [XmlElement("app:control")] public readonly AppControl AppControl = new(false, false);
 
-        [XmlArray("hatenablog:custom-url")]
+        [XmlElement("hatenablog:custom-url")]
         public readonly CustomUrl CustomUrl = new(header.UrlPath);
     }
 
     public class Author(string name)
     {
-        public readonly string Name = name;
+        [XmlElement("name")] public readonly string Name = name;
     }
 
     public class Content(string content)
     {
-        [XmlAttribute] public readonly string Type = "text/plain";
-        [XmlElement] public readonly string Element = content;
+        [XmlAttribute("type")] public readonly string Type = "text/plain";
+        [XmlText] public readonly string Text = content;
     }
 
     public class Category(string[] category)
     {
-        [XmlAttribute] public readonly string[] Term = category;
+        [XmlAttribute("term")] public readonly string[] Term = category;
     }
 
     public class AppControl(bool draft, bool preview)
     {
-        [XmlArray("app:draft")] public readonly string Draft = draft ? "yes" : "no";
-        [XmlArray("app:preview")] public readonly string Preview = preview ? "yes" : "no";
+        [XmlElement("app:draft")] public readonly string Draft = draft ? "yes" : "no";
+        [XmlElement("app:preview")] public readonly string Preview = preview ? "yes" : "no";
     }
 
     public class CustomUrl(string urlPath)
@@ -58,7 +61,7 @@ internal static class PostRequestSchema
         [XmlAttribute("xmlns:hatenablog")]
         public readonly string HatenaBlog = "http://www.hatena.ne.jp/info/xmlns#hatenablog";
 
-        [XmlElement] public readonly string Element = urlPath;
+        [XmlText] public readonly string Text = urlPath;
     }
 }
 
