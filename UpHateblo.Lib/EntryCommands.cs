cb10b78d52c71bcd4cf795eddd1994c26dd3bee2
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-09-24 13:16:59 +0900

    update: 外部から使用しやすいAPIにするためにコマンドを静的クラスの静的メソッドに変更

---
+++ UpHateblo.Lib/EntryCommands.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics.CodeAnalysis;
+using UpHateblo.Lib.Entities;
+using UpHateblo.Lib.Http;
+using UpHateblo.Lib.Schema;
+
+namespace UpHateblo.Lib;
+
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public static class EntryCommands
+{
+    private static readonly PostEntrySchema PostEntrySchema = new();
+
+    public static async Task Post(
+        HttpClient httpClient,
+        BlogConfig blog,
+        EntryHeader header,
+        string content,
+        string? wsseNonce = null,
+        DateTime? wsseDateTime = null
+    )
+    {
+        var httpContent =
+            GenHatenaContent(PostEntrySchema, blog, header, content, wsseNonce, wsseDateTime);
+        var res = await httpClient.PostAsync(blog.EntryEndPoint, httpContent);
+        res.EnsureSuccessStatusCode();
+    }
+
+    private static HatenaContent GenHatenaContent(
+        EntrySchemaBase? schema,
+        BlogConfig blog,
+        EntryHeader header,
+        string content,
+        string? wsseNonce = null,
+        DateTime? wsseDateTime = null
+    )
+    {
+        var entry = new Entry(blog, header, content);
+        var xml = schema?.Serialize(entry);
+        var wsse = new Wsse(
+            entry.Blog.Username,
+            entry.Blog.Password,
+            wsseNonce ?? Guid.NewGuid().ToString(),
+            wsseDateTime ?? DateTime.Now
+        );
+        return new HatenaContent(xml, wsse);
+    }
+}
