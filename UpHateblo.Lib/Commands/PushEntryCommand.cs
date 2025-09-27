2f2fff17d914a8fd3ca2018ffb2aafce6b0d9f8a
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-09-27 15:04:02 +0900

    feat: PushEntryCommandクラスを新規作成し既存エントリの更新機能を実装

---
+++ UpHateblo.Lib/Commands/PushEntryCommand.cs
@@ -0,0 +1,25 @@
+using UpHateblo.Lib.Entities;
+using UpHateblo.Lib.Schema;
+
+namespace UpHateblo.Lib.Commands;
+
+public static class PushEntryCommand
+{
+    private static readonly PostEntrySchema PostEntrySchema = new();
+
+    public static async Task Run(
+        HttpClient httpClient,
+        BlogConfig blog,
+        string entryId,
+        Entry entry,
+        string? wsseNonce = null,
+        DateTime? wsseDateTime = null
+    )
+    {
+        var httpContent =
+            CommandHelper.GenHatenaContent(PostEntrySchema, blog, entry, wsseNonce, wsseDateTime);
+        var memberEndpoint = new Uri($"{blog.EntryEndPoint}/{entryId}");
+        var res = await httpClient.PutAsync(memberEndpoint, httpContent);
+        res.EnsureSuccessStatusCode();
+    }
+}
