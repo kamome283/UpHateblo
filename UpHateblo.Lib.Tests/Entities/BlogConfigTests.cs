68f9e74d629e4b4b056b068947d252ce8667a587
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-09-30 09:47:47 +0900

    refactor: BlogConfigTests

--- UpHateblo.Lib.Tests/Entities/BlogConfigTests.cs
+++ UpHateblo.Lib.Tests/Entities/BlogConfigTests.cs
@@ -4,12 +4,12 @@ namespace UpHateblo.Lib.Tests.Entities;
 
 public class BlogConfigTests
 {
-    [Theory]
-    [InlineData("kamome283.hatenablog.com", "Kamome283",
-        "https://blog.hatena.ne.jp/Kamome283/kamome283.hatenablog.com/atom/entry")]
-    public void EntryEndPointIsCorrect(string blogId, string userName, string expected)
+    [Fact]
+    public void IsCorrectEntryEndPoint()
     {
-        var blog = new BlogConfig(blogId, userName, "");
-        Assert.Equal(expected, blog.EntryEndPoint.AbsoluteUri);
+        var blog = new BlogConfig("kamome283.hatenablog.com", "Kamome283", "");
+        var expected =
+            new Uri("https://blog.hatena.ne.jp/Kamome283/kamome283.hatenablog.com/atom/entry");
+        Assert.Equal(expected, blog.EntryEndPoint);
     }
 }
