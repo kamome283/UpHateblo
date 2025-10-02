79d33a6e0fba6a6fb7d566915dea55b9cafc686a
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-10-02 15:59:01 +0900

    feat: WebRequestTestBaseのティアダウンで指定したIDのエントリーを削除する後処理を実装

--- UpHateblo.Lib.Tests/Shared/WebRequestTestBase.cs
+++ UpHateblo.Lib.Tests/Shared/WebRequestTestBase.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Configuration;
+using UpHateblo.Lib.Entry.Delete;
 using UpHateblo.Lib.Shared;
 
 namespace UpHateblo.Lib.Tests.Shared;
@@ -9,12 +10,15 @@ public record BlogConfigSecrets(BlogConfig Blog);
 
 [Collection("WebRequest")]
 [Trait("Category", "WebRequest")]
-public abstract class WebRequestTestBase
+public abstract class WebRequestTestBase : IDisposable
 {
     // TODO: このプロパティはEntryのコマンドに関するものなので、より適切な場所に移動させたい
     // この名前空間のテストは結合テストでないので、テスト対象のエントリーは決め打ちにしている
     protected const string FixedTargetEntryId = "6802888565257240236";
     protected static readonly HttpClient HttpClient = new();
+
+    // 複数回Disposeが呼ばれても問題ないようにQueueで管理する
+    protected readonly Queue<string> EntryIdsToDispose = [];
 
     protected WebRequestTestBase()
     {
@@ -29,4 +33,14 @@ public abstract class WebRequestTestBase
     }
 
     protected BlogConfig BlogConfig { get; }
+
+    public void Dispose()
+    {
+        while (EntryIdsToDispose.TryDequeue(out var entryId))
+        {
+            // Task.WaitAllは一度にすべてのリクエストを送ってしまうのでサーバーに負荷をかけてしまう。
+            // そのため一件ずつDeleteEntryを走らせている
+            DeleteEntry.Run(HttpClient, BlogConfig, entryId).Wait();
+        }
+    }
 }
