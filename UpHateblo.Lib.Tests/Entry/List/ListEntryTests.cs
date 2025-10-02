df327cd49cb245a484f32540ffab50e7f35da4bf
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-10-02 17:19:50 +0900

    refactor: ListEntryコマンドのテストが常にパジネーションが必要になるようセットアップ

--- UpHateblo.Lib.Tests/Entry/List/ListEntryTests.cs
+++ UpHateblo.Lib.Tests/Entry/List/ListEntryTests.cs
@@ -1,14 +1,31 @@
 using UpHateblo.Lib.Entry.List;
+using UpHateblo.Lib.Entry.Post;
+using UpHateblo.Lib.Tests.Entry.Shared;
 using UpHateblo.Lib.Tests.Shared;
 
 namespace UpHateblo.Lib.Tests.Entry.List;
 
 public class ListEntryTests : WebRequestTestBase
 {
+    public ListEntryTests()
+    {
+        foreach (var _ in Enumerable.Range(0, 10))
+        {
+            PostAndQueueEntry().Wait();
+        }
+    }
+
     [Fact]
     public async Task ItCanListEntries()
     {
         var entries = await ListEntry.Run(HttpClient, BlogConfig);
         Assert.Contains(entries, e => e.EntryId == FixedTargetEntryId);
+    }
+
+    private async Task PostAndQueueEntry()
+    {
+        var entry = EntityExamples.PostableEntryExample();
+        var res = await PostEntry.Run(HttpClient, BlogConfig, entry);
+        EntryIdsToDispose.Enqueue(res.EntryId);
     }
 }
