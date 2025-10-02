3da4aebdbaf48f6242466965fa3cca531a469427
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-10-02 11:32:18 +0900

    fix: ListEntryコマンドがレスポンスのパジネーションに対応していない問題を修正
    既存のテストケースでもパジネーションが存在するときにエラーが発生していたので、テストケースに変更はない。

--- UpHateblo.Lib/Entry/List/ListEntry.cs
+++ UpHateblo.Lib/Entry/List/ListEntry.cs
@@ -6,8 +6,27 @@ namespace UpHateblo.Lib.Entry.List;
 
 public static class ListEntry
 {
-    // TODO: ペジネーションに対応させる
-    public static async Task<FetchedEntry[]> Run(
+    public static async Task<List<FetchedEntry>> Run(
+        HttpClient httpClient,
+        BlogConfig blog,
+        string? wsseNonce = null,
+        DateTime? wsseDateTime = null
+    )
+    {
+        Uri? next = blog.EntryEndPoint;
+        List<FetchedEntry> results = [];
+        while (next != null)
+        {
+            var (n, e) = await RunSingleIteration(next, httpClient, blog, wsseNonce, wsseDateTime);
+            next = n;
+            results.AddRange(e);
+        }
+
+        return results;
+    }
+
+    private static async Task<(Uri? next, IEnumerable<FetchedEntry> entries)> RunSingleIteration(
+        Uri endpoint,
         HttpClient httpClient,
         BlogConfig blog,
         string? wsseNonce = null,
@@ -15,14 +34,16 @@ public static class ListEntry
     )
     {
         var wsse = WsseHelper.GenerateWsse(blog, wsseNonce, wsseDateTime);
-        var request = new HatenaRequest(HttpMethod.Get, blog.EntryEndPoint, null, wsse);
+        var request = new HatenaRequest(HttpMethod.Get, endpoint, null, wsse);
 
         var res = await httpClient.SendAsync(request);
         res.EnsureSuccessStatusCode();
 
         var content = await res.Content.ReadAsStringAsync();
         var xml = XDocument.Parse(content);
-        var entries = xml.Root!.Elements(AtomNs + "entry").Select(FetchedEntrySchema.Deserialize);
-        return entries.ToArray();
+        var root = xml.Root!;
+        var next = NextUriSchema.Deserialize(root);
+        var entries = root.Elements(AtomNs + "entry").Select(FetchedEntrySchema.Deserialize);
+        return (next, entries);
     }
 }
