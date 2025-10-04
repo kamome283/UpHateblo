5e358924010fd5a78f461fb94f0b1321f6cb742d
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-10-04 16:35:54 +0900

    refactor: テストコードの実装

--- UpHateblo.Lib.Tests/Entry/Fetch/FetchEntryTests.cs
+++ UpHateblo.Lib.Tests/Entry/Fetch/FetchEntryTests.cs
@@ -7,25 +7,13 @@ namespace UpHateblo.Lib.Tests.Entry.Fetch;
 
 public class FetchEntryTests : WebRequestTestBase
 {
-    private static readonly PostableEntry Postable =
-        EntityExamples.PostableEntryExample() with { Date = DateTime.Today };
-
-    private static string? _entryId;
-
-    public FetchEntryTests()
-    {
-        Task.Run(async () =>
-        {
-            var entry = await PostEntry.Run(HttpClient, BlogConfig, Postable);
-            _entryId = entry.EntryId;
-            EntryIdsToDispose.Enqueue(entry.EntryId);
-        }).Wait();
-    }
-
     [Fact]
     public async Task ItCanFetchEntry()
     {
-        var fetched = await FetchEntry.Run(HttpClient, BlogConfig, _entryId!);
-        Assert.Equal(Postable, fetched);
+        var posted =
+            await PostEntry.Run(HttpClient, BlogConfig, EntityExamples.PostableEntryExample());
+        EntryIdsToDispose.Enqueue(posted.EntryId);
+        var fetched = await FetchEntry.Run(HttpClient, BlogConfig, posted.EntryId);
+        Assert.Equal(posted, fetched);
     }
 }
