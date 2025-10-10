24d4da68e281d1b106c3b555a6f9cf4ddfa46c3b
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-10-10 15:38:08 +0900

    add: StringifyEntryに対応したConvertFrom-HatebloEntry

---
+++ UpHateblo.Cmdlets/ConvertFromHatebloEntry.cs
@@ -0,0 +1,29 @@
+using System.Management.Automation;
+using UpHateblo.Lib.Entry.Shared;
+using UpHateblo.Lib.Entry.Stringify;
+
+namespace UpHateblo.Pwsh;
+
+[Cmdlet("ConvertFrom", "HatebloEntry"), OutputType(typeof(string))]
+public class ConvertFromHatebloEntry : Cmdlet
+{
+    [Parameter(ValueFromPipeline = true, Mandatory = true)]
+    public MaybeEntry[] Entry { get; set; }
+
+    protected override void ProcessRecord()
+    {
+        foreach (var entry in Entry)
+        {
+            try
+            {
+                var body = StringifyEntry.Run(entry);
+                WriteObject(body);
+            }
+            catch (Exception ex)
+            {
+                WriteError(
+                    new ErrorRecord(ex, ex.GetType().Name, ErrorCategory.NotSpecified, entry));
+            }
+        }
+    }
+}
