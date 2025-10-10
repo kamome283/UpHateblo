ccfe03a143ec3aadaed7ca7ffbdbdc808fb539a8
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-10-10 15:32:22 +0900

    add: Edit-HatebloEntry

---
+++ UpHateblo.Cmdlets/EditHatebloEntry.cs
@@ -0,0 +1,57 @@
+using System.Management.Automation;
+using UpHateblo.Lib.Entry.Edit;
+using UpHateblo.Lib.Entry.Shared;
+
+namespace UpHateblo.Pwsh;
+
+[Cmdlet("Edit", "HatebloEntry")]
+public class EditHatebloEntry : WebRequestingCmdletBase
+{
+    private AsyncTaskHandler<EditableEntry, FetchedEntry, Exception> _taskHandler;
+    [Parameter(ValueFromPipeline = true, Mandatory = true)]
+    public MaybeEntry[] Entry { get; set; } = [];
+    [Parameter] public int Parallel { get; set; } = 1;
+
+    protected override void BeginProcessing()
+    {
+        base.BeginProcessing();
+        _taskHandler = new AsyncTaskHandler<EditableEntry, FetchedEntry, Exception>()
+        {
+            ParallelOptions = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = Parallel,
+                CancellationToken = CancellationToken
+            },
+            Body = async (entry, token) => await EditEntry.Run(HttpClient, BlogConfig, entry, token)
+        };
+    }
+
+    protected override void ProcessRecord()
+    {
+        foreach (var entry in Entry)
+        {
+            try
+            {
+                var editable = (EditableEntry)entry;
+                _taskHandler.Add(editable);
+            }
+            catch (Exception ex)
+            {
+                WriteError(
+                    new ErrorRecord(ex, ex.GetType().Name, ErrorCategory.NotSpecified, entry));
+            }
+        }
+    }
+
+    protected override void EndProcessing()
+    {
+        _taskHandler.CompleteAdding();
+        foreach (var (editable, fetched, ex) in _taskHandler.BlockingOutEnumerable)
+        {
+            if (ex is not null)
+                WriteError(
+                    new ErrorRecord(ex, ex.GetType().Name, ErrorCategory.NotSpecified, editable));
+            else WriteObject(fetched);
+        }
+    }
+}
